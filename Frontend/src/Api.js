c529f96ee8f98b0340ef1160f0c13492c976a6f5
Nishantt02 <nishantchauhannn@gmail.com>
2025-10-08 12:47:02 +0530

    updates

--- Frontend/src/Api.js
+++ Frontend/src/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Production URL via environment variable, fallback to localhost for local dev
 const API = axios.create({
-  baseURL: 'https://financial-trading-app-39.onrender.com/api',
+  baseURL: 'http://localhost:500/api',
   withCredentials: true // if using cookies/auth
 });
 
