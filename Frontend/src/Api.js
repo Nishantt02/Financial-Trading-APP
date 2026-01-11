8f9cf0b6f590324cce01b3c7375978b43ec3d6f3
Nishantt02 <nishantchauhannn@gmail.com>
2026-01-11 22:51:08 +0530

    deployed changes

--- Frontend/src/Api.js
+++ Frontend/src/Api.js
@@ -4,7 +4,14 @@ import axios from "axios";
 // Production URL via environment variable, fallback to localhost for local dev
 const API = axios.create({
   baseURL: 'https://financial-trading55.onrender.com/api',
-  withCredentials: true // if using cookies/auth
+ 
+});
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default API;
